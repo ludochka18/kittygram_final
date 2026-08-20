3469054ff1a9f2590a9ebf767e167c64409815d9
Ludmila Barsukova <ludmilabarsukova13@gmail.com>
2026-08-20 12:36:55 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,9 +2,15 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+load_dotenv(BASE_DIR.parent / .env) # найди .env в корне проекта
+
+SECRET_KEY = os.getenv('SECRET_KEY') or get_random_secret_key()
+# 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
