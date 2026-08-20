ca137194a0f93fa47e9d031f22956fd97dc3b8a4
Ludmila Barsukova <ludmilabarsukova13@gmail.com>
2026-08-20 18:01:00 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR.parent / .env) # найди .env в корне проекта
 
 SECRET_KEY = os.getenv('SECRET_KEY') or get_random_secret_key()
-# 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
